wjj-8283/open-wallpaper-engine

commit 979dd6f428ee44996d95746a21011f03bc6d3c61

build: use system dependencies for renderer

src/Vulkan/Shader.cpp
@@ -2,9 +2,6 @@
 
 #include <algorithm>
 #include <cassert>
-#include <glslang/Include/Types.h>
-#include <glslang/MachineIndependent/localintermediate.h>
-#include <glslang/MachineIndependent/iomapper.h>
 #include <glslang/SPIRV/GlslangToSpv.h>
 #include "Spv.hpp"
 #include "TextureCache.hpp"
@@ -125,14 +122,6 @@ inline void SetMessageOptions(const ShaderCompOpt& opt, EShMessages& emsg) {
     if (opt.suppress_warnings_glsl) emsg = (EShMessages)(emsg | EShMsgSuppressWarnings);
     if (getClient(opt.client_ver) == glslang::EShClientVulkan)
         emsg = (EShMessages)(emsg | EShMsgVulkanRules);
-}
-
-inline i32 GetTypeNum(const glslang::TType* type) {
-    i32 num { 1 };
-    if (type->isArray()) num *= type->getCumulativeArraySize();
-    if (type->isVector()) num *= type->getVectorSize();
-    if (type->isMatrix()) num *= type->getMatrixCols() * type->getMatrixRows();
-    return num;
 }
 } // namespace
 const TBuiltInResource wallpaper::vulkan::DefaultTBuiltInResource {
@@ -437,14 +426,7 @@ bool wallpaper::vulkan::CompileAndLinkShaderUnits(std::span<const ShaderCompUnit
         return false;
     }
 
-    for (auto& unit : compUnits) {
-        (void)program.getIntermediate(unit.stage);
-    }
-    glslang::TIntermediate*         firstIm = program.getIntermediate(compUnits[0].stage);
-    glslang::TDefaultGlslIoResolver resolver(*firstIm);
-    glslang::TGlslIoMapper          ioMapper;
-
-    if (! (program.mapIO(&resolver, &ioMapper))) {
+    if (! (program.mapIO())) {
         LOG_ERROR("glslang(mapIo): %s\n", program.getInfoLog());
         return false;
     }
@@ -459,7 +441,6 @@ bool wallpaper::vulkan::CompileAndLinkShaderUnits(std::span<const ShaderCompUnit
         Uni_ShaderSpv spv = std::make_unique<ShaderSpv>();
         spv->stage        = ::ToGeneType(::ToVkType(unit.stage));
         auto im           = program.getIntermediate(unit.stage);
-        im->setOriginUpperLeft();
         glslang::GlslangToSpv(*im, spv->spirv, &logger, &spvOptions);
         spvs.emplace_back(std::move(spv));
 
@@ -468,28 +449,4 @@ bool wallpaper::vulkan::CompileAndLinkShaderUnits(std::span<const ShaderCompUnit
     }
 
     return true;
-}
-
-VkFormat wallpaper::vulkan::ToVkType(glslang::TBasicType type, size_t size) {
-#define FORMAT_SWITCH(in, s, sign, type)                       \
-    switch (in) {                                              \
-    case 1: return _VK_FORMAT_1(s, _##sign, type, R);          \
-    case 2: return _VK_FORMAT_2(s, _##sign, type, R, G);       \
-    case 3: return _VK_FORMAT_3(s, _##sign, type, R, G, B);    \
-    case 4: return _VK_FORMAT_4(s, _##sign, type, R, G, B, A); \
-    }                                                          \
-    break;
-
-    switch (type) {
-    case glslang::TBasicType::EbtFloat: FORMAT_SWITCH(size, 32, S, FLOAT);
-    case glslang::TBasicType::EbtInt: FORMAT_SWITCH(size, 32, S, INT);
-    case glslang::TBasicType::EbtUint: FORMAT_SWITCH(size, 32, U, INT);
-    default: break;
-    }
-    LOG_ERROR("can't covert glslang type \"%s\" of size %d to vulkan format",
-              glslang::TType::getBasicString(type),
-              size);
-    assert(false);
-    return VK_FORMAT_UNDEFINED;
-#undef FORMAT_SWITCH
 }

src/CMakeLists.txt
@@ -1,6 +1,6 @@
 find_package(OpenGL REQUIRED)
 find_package(Threads REQUIRED)
-pkg_check_modules(LZ4 REQUIRED liblz4)
+pkg_check_modules(LZ4 REQUIRED IMPORTED_TARGET liblz4)
 pkg_check_modules(AVFORMAT REQUIRED IMPORTED_TARGET libavformat)
 pkg_check_modules(AVCODEC REQUIRED IMPORTED_TARGET libavcodec)
 pkg_check_modules(AVUTIL REQUIRED IMPORTED_TARGET libavutil)
@@ -106,7 +106,7 @@ set(InteralLib
 target_link_libraries(
   ${PROJECT_NAME}
   PUBLIC wpUtils
-  PRIVATE ${InteralLib} nlohmann_json quickjs::quickjs ${LZ4_LIBRARIES}
+  PRIVATE ${InteralLib} nlohmann_json::nlohmann_json quickjs::quickjs PkgConfig::LZ4
           PkgConfig::AVFORMAT
           PkgConfig::AVCODEC
           PkgConfig::AVUTIL

src/Vulkan/CMakeLists.txt
@@ -20,7 +20,7 @@ target_link_libraries(${LIB_NAME}
 PUBLIC
 	glslang
     spirv-reflect-static
-	${Vulkan_LIBRARIES}
+	Vulkan::Vulkan
     wpScene
 PRIVATE
 	SPIRV

third_party/CMakeLists.txt
@@ -1,49 +1,85 @@
 set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 
-set(JSON_BuildTests         OFF CACHE INTERNAL "")
-set(JSON_Install            OFF CACHE INTERNAL "")
 set(SPIRV_REFLECT_STATIC_LIB  ON  CACHE INTERNAL "")
 set(SPIRV_REFLECT_EXECUTABLE  OFF CACHE INTERNAL "")
 set(SPIRV_REFLECT_BUILD_TESTS OFF CACHE INTERNAL "")
 set(SPIRV_REFLECT_EXAMPLES    OFF CACHE INTERNAL "")
-set(SPIRV_REFLECT_INSTALL    OFF CACHE INTERNAL "")
-set(GLSLANG_TESTS           OFF CACHE INTERNAL "")
-set(GLSLANG_ENABLE_INSTALL  OFF CACHE INTERNAL "")
-set(SKIP_GLSLANG_INSTALL    ON  CACHE INTERNAL "")
-set(ENABLE_GLSLANG_BINARIES OFF CACHE INTERNAL "")
-set(ENABLE_SPVREMAPPER      OFF CACHE INTERNAL "")
-set(ENABLE_GLSLANG_JS       OFF CACHE INTERNAL "")
-set(ENABLE_HLSL             OFF CACHE INTERNAL "")
-set(ENABLE_OPT              OFF CACHE INTERNAL "")
-set(ARGPARSE_INSTALL        OFF CACHE INTERNAL "")
-set(ARGPARSE_BUILD_TESTS    OFF CACHE INTERNAL "")
-set(ARGPARSE_BUILD_SAMPLES  OFF CACHE INTERNAL "")
-set(QJS_BUILD_EXAMPLES      OFF CACHE INTERNAL "")
-set(QJS_BUILD_WERROR        OFF CACHE INTERNAL "")
-set(QJS_BUILD_LIBC          OFF CACHE INTERNAL "")
-set(QJS_BUILD_CLI_STATIC    OFF CACHE INTERNAL "")
-set(QJS_BUILD_CLI_WITH_MIMALLOC OFF CACHE INTERNAL "")
-set(QJS_BUILD_CLI_WITH_STATIC_MIMALLOC OFF CACHE INTERNAL "")
+set(SPIRV_REFLECT_INSTALL     OFF CACHE INTERNAL "")
 
-# ------------------------------------------------------------------
-# Locally vendored dependencies.
-# ------------------------------------------------------------------
+find_package(PkgConfig REQUIRED)
+find_package(Eigen3 CONFIG REQUIRED)
+find_package(nlohmann_json CONFIG REQUIRED)
+find_package(argparse CONFIG REQUIRED)
+
+if(NOT qjs_DIR)
+    foreach(_prefix IN LISTS CMAKE_PREFIX_PATH)
+        set(_quickjs_config_dir "${_prefix}/lib/cmake/quickjs")
+        if(EXISTS "${_quickjs_config_dir}/qjsConfig.cmake")
+            set(qjs_DIR "${_quickjs_config_dir}" CACHE PATH "Path to qjsConfig.cmake")
+            break()
+        endif()
+    endforeach()
+endif()
+
+if(NOT qjs_DIR)
+    find_path(_quickjs_include_dir
+        NAMES quickjs.h
+        PATHS ${CMAKE_PREFIX_PATH}
+        PATH_SUFFIXES include include/quickjs)
+    if(_quickjs_include_dir)
+        if(_quickjs_include_dir MATCHES "/include/quickjs$")
+            get_filename_component(_quickjs_prefix "${_quickjs_include_dir}/../.." ABSOLUTE)
+        else()
+            get_filename_component(_quickjs_prefix "${_quickjs_include_dir}/.." ABSOLUTE)
+        endif()
+
+        set(_quickjs_config_dir "${_quickjs_prefix}/lib/cmake/quickjs")
+        if(EXISTS "${_quickjs_config_dir}/qjsConfig.cmake")
+            set(qjs_DIR "${_quickjs_config_dir}" CACHE PATH "Path to qjsConfig.cmake")
+        endif()
+    endif()
+endif()
+
+find_package(qjs CONFIG REQUIRED)
+find_package(glslang CONFIG REQUIRED)
+
+function(promote_imported_target target_name)
+    if(TARGET ${target_name})
+        get_target_property(_is_imported ${target_name} IMPORTED)
+        if(_is_imported)
+            set_property(TARGET ${target_name} PROPERTY IMPORTED_GLOBAL TRUE)
+        endif()
+    endif()
+endfunction()
+
+promote_imported_target(Eigen3::Eigen)
+promote_imported_target(nlohmann_json::nlohmann_json)
+promote_imported_target(qjs)
+promote_imported_target(qjs::qjs)
+promote_imported_target(glslang::glslang)
+promote_imported_target(glslang::SPIRV)
+
 add_subdirectory(glad)
-add_subdirectory(nlohmann_json EXCLUDE_FROM_ALL)
-add_subdirectory(argparse EXCLUDE_FROM_ALL)
 add_subdirectory(spirv_reflect EXCLUDE_FROM_ALL)
-add_subdirectory(glslang EXCLUDE_FROM_ALL)
-add_subdirectory(quickjs_ng EXCLUDE_FROM_ALL)
+
+if(TARGET nlohmann_json::nlohmann_json AND NOT TARGET nlohmann_json)
+    add_library(nlohmann_json INTERFACE)
+    target_link_libraries(nlohmann_json INTERFACE nlohmann_json::nlohmann_json)
+endif()
 
 if(TARGET qjs AND NOT TARGET quickjs::quickjs)
     add_library(quickjs::quickjs ALIAS qjs)
+elseif(TARGET qjs::qjs AND NOT TARGET quickjs::quickjs)
+    add_library(quickjs::quickjs ALIAS qjs::qjs)
 endif()
 
-# Eigen is header-only for renderer usage.
-add_library(eigen INTERFACE)
-add_library(Eigen3::Eigen ALIAS eigen)
-target_include_directories(eigen SYSTEM INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/eigen)
+if(TARGET glslang::glslang AND NOT TARGET glslang)
+    add_library(glslang ALIAS glslang::glslang)
+endif()
 
-# miniaudio is consumed as <miniaudio.h> and <extras/stb_vorbis.c>.
+if(TARGET glslang::SPIRV AND NOT TARGET SPIRV)
+    add_library(SPIRV ALIAS glslang::SPIRV)
+endif()
+
 add_library(miniaudio INTERFACE)
 target_include_directories(miniaudio SYSTEM INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/miniaudio)

src/Vulkan/include/Vulkan/Shader.hpp
@@ -1,14 +1,11 @@
 #pragma once
 #include "Instance.hpp"
 #include "ShaderComp.hpp"
-#include <glslang/Include/BaseTypes.h>
 
 namespace wallpaper
 {
 namespace vulkan
 {
-
-VkFormat ToVkType(glslang::TBasicType, size_t);
 
 struct ShaderReflected {
     struct BlockedUniform {

commit b1ac7344f53d4b9857fcf261b853e8f43fb2aa0b

fix(shader): preserve linked stage IO mapping
Fix the shader compilation pipeline regression introduced by 979dd6f428ee44996d95746a21011f03bc6d3c61 by forcing glslang to build stage intermediates before mapping IO with an explicit mapper.

Add coverage that reflected varying locations match across linked vertex and fragment stages.

src/Vulkan/Shader.cpp
@@ -426,7 +426,14 @@ bool wallpaper::vulkan::CompileAndLinkShaderUnits(std::span<const ShaderCompUnit
         return false;
     }
 
-    if (! (program.mapIO())) {
+    for (auto& unit : compUnits) {
+        (void)program.getIntermediate(unit.stage);
+    }
+
+    std::unique_ptr<glslang::TIoMapResolver> resolver(program.getGlslIoResolver(compUnits[0].stage));
+    std::unique_ptr<glslang::TIoMapper>      ioMapper(glslang::GetGlslIoMapper());
+
+    if (! (program.mapIO(resolver.get(), ioMapper.get()))) {
         LOG_ERROR("glslang(mapIo): %s\n", program.getInfoLog());
         return false;
     }

tests/CMakeLists.txt
@@ -85,7 +85,7 @@ if(APPLE)
             -P ${CMAKE_CURRENT_SOURCE_DIR}/audio/coreaudio_backend_config.cmake)
 endif()
 add_executable(shader_cache_metadata_test shader_cache_metadata_test.cpp)
-target_link_libraries(shader_cache_metadata_test PRIVATE ${PROJECT_NAME} wpFs wpScene GTest::gtest_main)
+target_link_libraries(shader_cache_metadata_test PRIVATE ${PROJECT_NAME} wpFs wpScene wpVulkan GTest::gtest_main)
 target_include_directories(shader_cache_metadata_test PRIVATE .)
 target_compile_features(shader_cache_metadata_test PRIVATE cxx_std_20)
 gtest_discover_tests(

tests/shader_cache_metadata_test.cpp
@@ -1,6 +1,9 @@
 #include "Fs/PhysicalFs.h"
 #include "Fs/VFS.h"
 #include "WPShaderParser.hpp"
+#include "Vulkan/ShaderComp.hpp"
+
+#include <spirv_reflect.h>
 
 #include <gtest/gtest.h>
 
@@ -63,6 +66,44 @@ void main() {
     return units;
 }
 
+std::uint32_t ReflectedOutputLocation(const std::vector<unsigned int>& spirv,
+                                      const char*                      name) {
+    spv_reflect::ShaderModule module(spirv, SPV_REFLECT_MODULE_FLAG_NO_COPY);
+
+    std::uint32_t output_count = 0;
+    EXPECT_EQ(module.EnumerateOutputVariables(&output_count, nullptr), SPV_REFLECT_RESULT_SUCCESS);
+
+    std::vector<SpvReflectInterfaceVariable*> outputs(output_count);
+    EXPECT_EQ(module.EnumerateOutputVariables(&output_count, outputs.data()),
+              SPV_REFLECT_RESULT_SUCCESS);
+
+    for (const auto* output : outputs) {
+        if (output == nullptr || output->name == nullptr) continue;
+        if (std::string(output->name) == name) return output->location;
+    }
+
+    return std::numeric_limits<std::uint32_t>::max();
+}
+
+std::uint32_t ReflectedInputLocation(const std::vector<unsigned int>& spirv,
+                                     const char*                      name) {
+    spv_reflect::ShaderModule module(spirv, SPV_REFLECT_MODULE_FLAG_NO_COPY);
+
+    std::uint32_t input_count = 0;
+    EXPECT_EQ(module.EnumerateInputVariables(&input_count, nullptr), SPV_REFLECT_RESULT_SUCCESS);
+
+    std::vector<SpvReflectInterfaceVariable*> inputs(input_count);
+    EXPECT_EQ(module.EnumerateInputVariables(&input_count, inputs.data()),
+              SPV_REFLECT_RESULT_SUCCESS);
+
+    for (const auto* input : inputs) {
+        if (input == nullptr || input->name == nullptr) continue;
+        if (std::string(input->name) == name) return input->location;
+    }
+
+    return std::numeric_limits<std::uint32_t>::max();
+}
+
 TEST(ShaderCacheMetadataTest, CacheHitStillPopulatesActiveTextureSlots) {
     GlslangEnvironment glslang;
     const auto         temp = MakeTempDir();
@@ -90,6 +131,56 @@ TEST(ShaderCacheMetadataTest, CacheHitStillPopulatesActiveTextureSlots) {
         ASSERT_TRUE(WPShaderParser::CompileToSpv("demo-scene", units, codes, vfs, &info, textures));
         ASSERT_TRUE(units[1].preprocess_info.active_tex_slots.contains(0));
     }
+}
+
+TEST(ShaderCacheMetadataTest, AutoMappedVaryingLocationsMatchAcrossStages) {
+    GlslangEnvironment glslang;
+
+    std::vector<vulkan::ShaderCompUnit> units {
+        vulkan::ShaderCompUnit {
+            .stage = EShLangVertex,
+            .src   = R"(
+#version 330
+layout(location = 0) in vec3 a_Position;
+out vec4 v_TexCoord;
+out vec2 v_MaskCoord;
+void main() {
+    v_TexCoord = vec4(0.0, 1.0, 0.0, 1.0);
+    v_MaskCoord = vec2(0.5, 0.25);
+    gl_Position = vec4(a_Position, 1.0);
+}
+)",
+        },
+        vulkan::ShaderCompUnit {
+            .stage = EShLangFragment,
+            .src   = R"(
+#version 330
+in vec2 v_MaskCoord;
+in vec4 v_TexCoord;
+out vec4 glOutColor;
+void main() {
+    glOutColor = vec4(v_TexCoord.xy, v_MaskCoord.xy);
+}
+)",
+        },
+    };
+
+    vulkan::ShaderCompOpt opt;
+    opt.client_ver             = glslang::EShTargetVulkan_1_1;
+    opt.auto_map_bindings      = true;
+    opt.auto_map_locations     = true;
+    opt.relaxed_errors_glsl    = true;
+    opt.relaxed_rules_vulkan   = true;
+    opt.suppress_warnings_glsl = true;
+
+    std::vector<vulkan::Uni_ShaderSpv> spvs;
+    ASSERT_TRUE(vulkan::CompileAndLinkShaderUnits(units, opt, spvs));
+    ASSERT_EQ(spvs.size(), 2u);
+
+    EXPECT_EQ(ReflectedOutputLocation(spvs[0]->spirv, "v_TexCoord"),
+              ReflectedInputLocation(spvs[1]->spirv, "v_TexCoord"));
+    EXPECT_EQ(ReflectedOutputLocation(spvs[0]->spirv, "v_MaskCoord"),
+              ReflectedInputLocation(spvs[1]->spirv, "v_MaskCoord"));
 }
 
 } // namespace